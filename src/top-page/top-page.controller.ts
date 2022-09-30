b94865bb3f57d18aafcac539af30d7cb5cc9d21e
shelludko <v.sheludko@tg.dunice.net>
2022-09-30 10:43:10 +0300

    for review

---
+++ src/top-page/top-page.controller.ts
@@ -0,0 +1,85 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { IdValidationPipe } from 'src/pipes/id-validation.pipe';
+import { CreateTopPageDto } from './dto/create-top-page.dto';
+import { FindTopPageDto } from './dto/find-top-page.dto';
+import { TOP_PAGE_NOT_FOUND_ERROR } from './top-page.constants';
+import { TopPageModel } from './top-page.model';
+import { TopPageService } from './top-page.service';
+
+@Controller('top-page')
+export class TopPageController {
+  constructor(private readonly topPageService: TopPageService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  async get(@Param('id', IdValidationPipe) id: string) {
+    const topPage = await this.topPageService.findById(id);
+    if (!topPage) {
+      throw new NotFoundException(TOP_PAGE_NOT_FOUND_ERROR);
+    }
+    return topPage;
+  }
+
+  @Get('byAlias/:alias')
+  async getByAlias(@Param('alias') alias: string) {
+    const topPage = await this.topPageService.findByAlias(alias);
+    if (!topPage) {
+      throw new NotFoundException(TOP_PAGE_NOT_FOUND_ERROR);
+    }
+    return topPage;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('create')
+  async create(@Body() dto: CreateTopPageDto) {
+    return this.topPageService.create(dto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id')
+  async patch(
+    @Param('id', IdValidationPipe) id: string,
+    @Body() dto: CreateTopPageDto,
+  ) {
+    const updatedTopPage = await this.topPageService.updateById(id, dto);
+    if (!updatedTopPage) {
+      throw new NotFoundException(TOP_PAGE_NOT_FOUND_ERROR);
+    }
+    return updatedTopPage;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  async delete(@Param('id', IdValidationPipe) id: string) {
+    const deletedTopPage = await this.topPageService.deleteById(id);
+    if (!deletedTopPage) {
+      throw new NotFoundException(TOP_PAGE_NOT_FOUND_ERROR);
+    }
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('find')
+  async find(@Body() dto: FindTopPageDto) {
+    return this.topPageService.findByCategory(dto.firstCategory);
+  }
+
+  @Get('textSearch/:text')
+  async textSearch(@Param('text') text: string) {
+    return this.topPageService.findByText(text);
+  }
+}
